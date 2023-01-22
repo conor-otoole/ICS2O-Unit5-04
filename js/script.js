4470b91c7c6993ffce80800ca99d80ae94bde084
conor-otoole <113055588+conor-otoole@users.noreply.github.com>
2023-01-22 15:11:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Conor All rights reserved
 //
 // Created by: conor-otoole
-// Created on: Sep 2020
+// Created on: Jan 2023
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-04/sw.js", {
+    scope: "/ICS2O-Unit5-04/",
   })
 }
 
-/**
- * This function displays an alert.
- */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML =
+    "Your age: " + valueFromSlider
+}
+
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  const day = document.getElementById("day").value
+  const age = document.getElementById("slider-value").value
+
+  if ((day == "tuesday" || day == "thursday") || (age > "12" || age < "25")) {
+    document.getElementById("price").innerHTML =
+      "You're eligible for student pricing."
+  } else {
+    document.getElementById("price").innerHTML =
+      "You're not eligible for student pricing."
+  }
 }
